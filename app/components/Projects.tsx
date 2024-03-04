2595a77284d6fc8628dbcac9bb409dcb91eb0f7f
Andrew Aghoghovwia <androyt1@gmail.com>
2024-03-04 11:35:13 +0000

    reduced lighten

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,9 +11,9 @@ const Projects = () => {
             <Canvas
                 camera={{ fov: 70, near: 0.1, far: 1000, position: [-5, 5, 18] }}
                 className='w-[50%] h-[50vh] bg-[#1E293B] flex justify-center items-center border-4 border-white'>
-                <directionalLight position={[5, 5, 0]} intensity={5} />
-                <ambientLight intensity={5} />
-                <spotLight intensity={4} />
+                <directionalLight position={[5, 5, 0]} intensity={3} />
+                <ambientLight intensity={2} />
+
                 <OrbitControls enableZoom={false} autoRotate={true} />
                 <Suspense
                     fallback={
